1f3e0e3fb36fb0c6300e84dbb76024bb43b0bc18
jslee6091 <jslee6091@naver.com>
2021-01-13 18:14:49 +0900

    django practce

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
@@ -20,3 +20,9 @@ def post_list_response(request):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+# Post 상세정보
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
