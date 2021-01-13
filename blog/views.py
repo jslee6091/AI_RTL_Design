dff753ddbbad00eb11ca5c2d03c154b1717fbec2
jslee6091 <jslee6091@naver.com>
2021-01-13 14:49:33 +0900

    django practce

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+
+def post_list_response(request):
+    name = 'Django'
+    response = HttpResponse(f'<h2>Hello {name}!!</h2>', content_type="text/html")
+    response.write(f'<h2>Hello {name}!!</h2>')
+    response.write(f'<p>HTTP METHOD : {request.method}</p>')
+    response.write(f'<p>HTTP ContentType : {request.content_type}</p>')
+    return response
+
+    # 또는 response 만들지 않고 return 문 하나로 끝내도 됨
+    # return HttpResponse(f'''<h2>Hello {name}!!</h2><p>HTTP METHOD : {request.method}</p>''')
+
+
+# Post 목록
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
